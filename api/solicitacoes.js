c73b523f37c2af39c4df3adbb0b9b58c9917496d
TheusSZIM <matheusborcate20@gmail.com>
2025-07-20 12:34:00 -0300

    Delete api directory

--- api/solicitacoes.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  const response = await fetch('https://script.google.com/macros/s/AKfycby5rUjZ3mP6lPMFn7ywEQcwxOGZ0nyznhffIctsLM2ac0NypulyrCmkHusVtl_V9caUFQ/exec
-');
-  const data = await response.json();
-
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.status(200).json(data);
-}
