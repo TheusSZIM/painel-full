7e93e0aa3c79ae08d6ddad709745d74fe15b41c8
TheusSZIM <matheusborcate20@gmail.com>
2025-07-20 12:22:26 -0300

    Update solicitacoes.js

--- api/solicitacoes.js
+++ api/solicitacoes.js
@@ -1,5 +1,6 @@
 export default async function handler(req, res) {
-  const response = await fetch('https://script.google.com/macros/s/AKfycbxU6-kwa96BmNMv78tUJFhQhmkVGJy_yuU8E8z8eV6mTaWrFhHw/exec');
+  const response = await fetch('https://script.google.com/macros/s/AKfycby5rUjZ3mP6lPMFn7ywEQcwxOGZ0nyznhffIctsLM2ac0NypulyrCmkHusVtl_V9caUFQ/exec
+');
   const data = await response.json();
 
   res.setHeader("Access-Control-Allow-Origin", "*");
